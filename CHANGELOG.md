a02c5c43e7e8fec644dd787731923651ec8cdaff
Jens Walter <jens@apimeister.com>
2023-01-28 12:24:03 +0100

    fix serializer

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.2.0 2023-01-28
+
+* fix edifact serializer
+
 # 0.1.0 2022-11-19
 
 * initial version
--- src/ser.rs
+++ src/ser.rs
@@ -1,48 +1,51 @@
 use crate::error::Error;
 use serde::{ser, Serialize};
 
+#[derive(Debug, Default)]
 pub struct Serializer {
-    output: Vec<Vec<Option<String>>>,
+    root_count: usize,
+    root_name: &'static str,
+    lines: Vec<String>,
+    segment_data: Vec<String>,
+    segment_count: usize,
+    inline: Option<usize>,
+    inline_data: Vec<String>,
 }
 
 pub fn to_string<T>(value: &T) -> Result<String, Error>
 where
     T: Serialize,
 {
-    let mut serializer = Serializer { output: vec![] };
+    let mut serializer = Serializer::default();
     value.serialize(&mut serializer)?;
     let mut final_string = "".to_string();
-    for line in serializer.output {
-        if line.len() == 1 {
+    #[cfg(feature = "debug")]
+    println!("debug vec: {serializer:?}");
+    // look for last segment
+    if !serializer.segment_data.is_empty() {
+        if !serializer.inline_data.is_empty() {
+            //merge inline data
+            let inline_data = serializer
+                .inline_data
+                .join(":")
+                .trim_end_matches(':')
+                .to_string();
+            //push to segment
+            serializer.segment_data.push(inline_data);
+            serializer.inline = None;
+            serializer.inline_data = vec![];
+        }
+        let last_line = serializer.segment_data.join("+");
+        serializer.segment_data = vec![];
+        serializer.segment_count = 0;
+        serializer.lines.push(last_line);
+    }
+    for line in serializer.lines {
+        let line = line.trim_end_matches('+');
+        if line.len() < 4 {
             continue;
         }
-        let mut x = line.clone();
-        x.reverse();
-        let mut length_of_line = x.len();
-        for item in x {
-            if item.is_none() {
-                length_of_line -= 1;
-            } else {
-                break;
-            }
-        }
-        let mut line = line.clone();
-        line.truncate(length_of_line);
-        //concatenate single line
-        let mut first_element = true;
-        for item in line {
-            match item {
-                Some(s) => final_string.push_str(&s),
-                None => final_string.push_str(""),
-            }
-            if first_element {
-                final_string.push('+');
-                first_element = false;
-            } else {
-                final_string.push(':');
-            }
-        }
-        final_string.pop();
+        final_string.push_str(line);
         final_string.push_str("'\n");
     }
     Ok(final_string)
@@ -52,28 +55,156 @@ impl<'a> ser::Serializer for &'a mut Serializer {
     type Ok = ();
     type Error = Error;
     fn serialize_str(self, v: &str) -> Result<Self::Ok, Self::Error> {
-        #[cfg(feature = "debug")]
-        println!("serialize_str: {}", v);
-        match self.output.last_mut() {
-            Some(el) => {
-                el.push(Some(v.to_string()));
+        if let Some(mut pos) = self.inline {
+            pos -= 1;
+            self.inline = Some(pos);
+            self.inline_data.push(v.to_string());
+            if pos == 0 {
+                //merge inline data
+                let inline_data = self.inline_data.join(":").trim_end_matches(':').to_string();
+                //push to segment
+                self.segment_data.push(inline_data);
+                self.inline = None;
+                self.inline_data = vec![];
+                self.segment_count -= 1;
             }
-            None => {
-                self.output.push(vec![Some(v.to_string())]);
+        } else {
+            self.segment_data.push(v.to_string());
+            self.segment_count -= 1;
+        }
+        Ok(())
+    }
+
+    fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
+        if let Some(mut pos) = self.inline {
+            pos -= 1;
+            self.inline = Some(pos);
+            self.inline_data.push("".to_string());
+            if pos == 0 {
+                //merge inline data
+                let inline_data = self.inline_data.join(":").trim_end_matches(':').to_string();
+                //push to segment
+                self.segment_data.push(inline_data);
+                self.inline = None;
+                self.inline_data = vec![];
+                self.segment_count -= 1;
+            }
+        } else {
+            self.segment_data.push("".to_string());
+            if self.segment_count > 0 {
+                self.segment_count -= 1;
             }
         }
         Ok(())
     }
-    fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
+
+    fn serialize_struct(
+        self,
+        name: &'static str,
+        len: usize,
+    ) -> Result<Self::SerializeStruct, Self::Error> {
         #[cfg(feature = "debug")]
-        println!("serialize_none");
-        match self.output.last_mut() {
-            Some(el) => el.push(None),
-            None => {
-                self.output.push(vec![None]);
-            }
+        println!(
+            "serialize_struct: {} {} segment:{:?}",
+            name, len, self.segment_data
+        );
+        // check for root
+        if self.root_count == 0 {
+            self.root_count = len;
+            self.root_name = name;
+            return Ok(self);
         }
+        // check for segment group
+        if name.starts_with(self.root_name) {
+            // must be new line
+            let last_line = self.segment_data.join("+");
+            self.segment_data = vec![];
+            self.inline_data = vec![];
+            self.segment_count = 0;
+            self.lines.push(last_line);
+        } else if self.segment_count > 0 {
+            // must be inline data
+            self.inline = Some(len);
+        } else {
+            // must be new line
+            let last_line = self.segment_data.join("+");
+            self.segment_data = vec![name.to_uppercase()];
+            self.inline_data = vec![];
+            self.inline = None;
+            self.segment_count = len;
+            self.lines.push(last_line);
+        }
+
+        Ok(self)
+    }
+
+    fn serialize_some<T: ?Sized>(self, value: &T) -> Result<Self::Ok, Self::Error>
+    where
+        T: Serialize,
+    {
+        #[cfg(feature = "debug")]
+        println!("serialize_some");
+        value.serialize(self)
+    }
+
+    fn serialize_unit_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        variant: &'static str,
+    ) -> Result<Self::Ok, Self::Error> {
+        #[cfg(feature = "debug")]
+        println!(
+            "serialize_unit_variant: {} {} {}",
+            _name, _variant_index, variant
+        );
+        self.serialize_str(variant)
+    }
+
+    fn serialize_newtype_struct<T: ?Sized>(
+        self,
+        _name: &'static str,
+        _value: &T,
+    ) -> Result<Self::Ok, Self::Error>
+    where
+        T: Serialize,
+    {
+        todo!()
+    }
+
+    fn serialize_newtype_variant<T: ?Sized>(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        value: &T,
+    ) -> Result<Self::Ok, Self::Error>
+    where
+        T: Serialize,
+    {
+        #[cfg(feature = "debug")]
+        println!(
+            "serialize_newtype_variant: {} {} {}",
+            _name, _variant_index, _variant
+        );
+        value.serialize(&mut *self)?;
         Ok(())
+    }
+
+    fn serialize_map(self, _len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
+        #[cfg(feature = "debug")]
+        println!("serialize_map: {:?}", _len);
+        Ok(self)
+    }
+
+    fn serialize_struct_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeStructVariant, Self::Error> {
+        todo!()
     }
 
     type SerializeSeq = Self;
@@ -128,98 +259,20 @@ impl<'a> ser::Serializer for &'a mut Serializer {
         todo!()
     }
 
-    fn serialize_char(self, v: char) -> Result<Self::Ok, Self::Error> {
-        #[cfg(feature = "debug")]
-        println!("serialize_char: {}", v);
-        match self.output.last_mut() {
-            Some(el) => el.push(Some(v.to_string())),
-            None => {
-                self.output.push(vec![Some(v.to_string())]);
-            }
-        }
-        Ok(())
+    fn serialize_char(self, _v: char) -> Result<Self::Ok, Self::Error> {
+        todo!()
     }
 
     fn serialize_bytes(self, _v: &[u8]) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
 
-    fn serialize_some<T: ?Sized>(self, value: &T) -> Result<Self::Ok, Self::Error>
-    where
-        T: Serialize,
-    {
-        #[cfg(feature = "debug")]
-        println!("serialize_some");
-        value.serialize(self)
-    }
-
     fn serialize_unit(self) -> Result<Self::Ok, Self::Error> {
-        #[cfg(feature = "debug")]
-        println!("serialize_unit");
-        //empty value
-        match self.output.last_mut() {
-            Some(el) => el.push(Some("".to_string())),
-            None => {
-                self.output.push(vec![Some("".to_string())]);
-            }
-        }
-        Ok(())
-    }
-
-    fn serialize_unit_struct(self, _name: &'static str) -> Result<Self::Ok, Self::Error> {
-        #[cfg(feature = "debug")]
-        println!("serialize_unit_struct: {}", _name);
-        match self.output.last_mut() {
-            Some(el) => el.push(Some("".to_string())),
-            None => {
-                self.output.push(vec![Some("".to_string())]);
-            }
-        }
-        Ok(())
-    }
-
-    fn serialize_unit_variant(
-        self,
-        _name: &'static str,
-        _variant_index: u32,
-        variant: &'static str,
-    ) -> Result<Self::Ok, Self::Error> {
-        #[cfg(feature = "debug")]
-        println!(
-            "serialize_unit_variant: {} {} {}",
-            _name, _variant_index, variant
-        );
-        self.serialize_str(variant)
-    }
-
-    fn serialize_newtype_struct<T: ?Sized>(
-        self,
-        _name: &'static str,
-        _value: &T,
-    ) -> Result<Self::Ok, Self::Error>
-    where
-        T: Serialize,
-    {
         todo!()
     }
 
-    fn serialize_newtype_variant<T: ?Sized>(
-        self,
-        _name: &'static str,
-        _variant_index: u32,
-        _variant: &'static str,
-        value: &T,
-    ) -> Result<Self::Ok, Self::Error>
-    where
-        T: Serialize,
-    {
-        #[cfg(feature = "debug")]
-        println!(
-            "serialize_newtype_variant: {} {} {}",
-            _name, _variant_index, _variant
-        );
-        value.serialize(&mut *self)?;
-        Ok(())
+    fn serialize_unit_struct(self, _name: &'static str) -> Result<Self::Ok, Self::Error> {
+        todo!()
     }
 
     fn serialize_seq(self, _len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
@@ -250,34 +303,6 @@ impl<'a> ser::Serializer for &'a mut Serializer {
         _len: usize,
     ) -> Result<Self::SerializeTupleVariant, Self::Error> {
         todo!()
-    }
-
-    fn serialize_map(self, _len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
-        #[cfg(feature = "debug")]
-        println!("serialize_map: {:?}", _len);
-        Ok(self)
-    }
-
-    fn serialize_struct_variant(
-        self,
-        _name: &'static str,
-        _variant_index: u32,
-        _variant: &'static str,
-        _len: usize,
-    ) -> Result<Self::SerializeStructVariant, Self::Error> {
-        todo!()
-    }
-
-    fn serialize_struct(
-        self,
-        name: &'static str,
-        _len: usize,
-    ) -> Result<Self::SerializeStruct, Self::Error> {
-        #[cfg(feature = "debug")]
-        println!("serialize_struct: {} {}", name, _len);
-        self.output
-            .push(vec![Some(name.to_uppercase().to_string())]);
-        Ok(self)
     }
 }
 
@@ -397,7 +422,6 @@ impl<'a> ser::SerializeStruct for &'a mut Serializer {
     {
         #[cfg(feature = "debug")]
         println!("serialize_field: {}", _key);
-        // self.output.push(vec![key.to_string()]);
         let _ = value.serialize(&mut **self);
         Ok(())
     }
